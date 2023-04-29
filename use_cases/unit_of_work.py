2f3beb219fa9a83d795af5f8be1e604f58e9bdb9
Patrick Kalkman <patrick@simpletechture.nl>
2023-04-29 08:27:04 +0200

    working on adding type hints

--- use_cases/unit_of_work.py
+++ use_cases/unit_of_work.py
@@ -1,3 +1,4 @@
+from types import TracebackType
 from adapters.base_connection import BaseConnection
 from adapters.base_repository import BaseRepository
 from domain.order import Order
@@ -5,24 +6,32 @@ from domain.person import Person
 
 
 class UnitOfWork:
-    def __init__(self, connection: BaseConnection,
-                 person_repository: BaseRepository[Person],
-                 order_repository: BaseRepository[Order]):
-        self.persons = person_repository
-        self.orders = order_repository
-        self.connection = connection
+    def __init__(
+        self,
+        connection: BaseConnection,
+        person_repository: BaseRepository[Person],
+        order_repository: BaseRepository[Order],
+    ) -> None:
+        self.persons: BaseRepository[Person] = person_repository
+        self.orders: BaseRepository[Order] = order_repository
+        self.connection: BaseConnection = connection
 
     def __enter__(self):
         return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
+    def __exit__(
+        self,
+        exc_type: type[BaseException],
+        exc_val: BaseException,
+        exc_tb: TracebackType,
+    ) -> None:
         if exc_type:
             self.rollback()
         else:
             self.commit()
 
-    def commit(self):
+    def commit(self) -> None:
         self.connection.commit()
 
-    def rollback(self):
+    def rollback(self) -> None:
         self.connection.rollback()
